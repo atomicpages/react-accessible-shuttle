2ae7a9e5c5eb3b62787302982ee664a60dc585fe
atomicpages <djtthompson@gmail.com>
2019-06-11 10:49:45 -0700

    Moving to lodash.compact
    * It's faster than Array.prototype.filter :|

--- src/utils/reducerUtils.js
+++ src/utils/reducerUtils.js
@@ -1,37 +1,17 @@
 // @flow
 
-/**
- * Removed falsy values form an array incrementally.
- *
- * Incrementally compact the array without iterating over every item in the source.
- * This method uses a little extra storage for a performance boost when reading
- * large lists.
- * @param {string[]} from
- * @param {string[]} to
- * @param {number[]} placeholders
- */
-function incrementalCompact(from: string[], to: string[], placeholders: number[]) {
-    for (let j = 0; j < placeholders.length; j++) {
-        if (!from[placeholders[j]]) {
-            from.splice(placeholders[j], 1);
-        } else {
-            // previous entry was removed so we need to percolate down
-            for (let k = placeholders[j]; k >= 0; k--) {
-                if (!from[k]) {
-                    from.splice(k, 1);
-                    break;
-                }
-            }
-        }
-    }
-}
+import compact from 'lodash.compact';
 
 /**
  * Moves only the selected items from source
  * to the target. This mutates the existing array
  * for better performance.
  */
-export const moveSelected = (from: string[], to: string[], selected: Set<number>) => {
+export const moveSelected = (
+    from: string[],
+    to: string[],
+    selected: Set<number>
+) => {
     const entries = selected.entries();
     const placeholders: number[] = [];
 
@@ -42,7 +22,8 @@ export const moveSelected = (from: string[], to: string[], selected: Set<number>
         to.push(from.splice(index, 1, null)[0]);
     }
 
-    incrementalCompact(from, to, placeholders);
+    // @see https://jsperf.com/inc-compact
+    return compact(from);
 };
 
 /**
